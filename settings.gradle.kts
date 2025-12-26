1f3ad57e91909ebc71be76af6b33b49d8cf1de95
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-12-26 11:20:33 +0100

    🔐 add temporary maven credentials

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.hytale-modding.info/releases") {
+            name = "HytaleModdingReleases"
+            // FIXME remove once maven becomes public
+            credentials {
+                username = "tmp_viewer"
+                password = "01KDD2ARZ795FTWBW0A97RP5RS"
+            }
+        }
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
